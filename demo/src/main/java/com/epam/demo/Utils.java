eb5a9b6ea20ab4bce7af5c37db4ff57eb21b34b7
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-03 13:46:25 +0400

    f f

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,13 +1,7 @@
 import java.util.List;
-
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         // Magic happens here using the StringUtils class from the 'utils' module
-        for (String arg : args) {
-            if (!StringUtils.isPositiveNumber(arg)) {
-                return false;
-            }
-        }
         return true;
     }
 }
